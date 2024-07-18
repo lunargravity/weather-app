b4389f2c869150f9f97d3f7fc0d62b8f0973f848
Anu Altankhuyag <aaltankhuyag@catalyte.io>
2024-07-18 11:22:39 -0400

    Working sidebar toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,46 +3,37 @@ import DateClass from './DateClass';
 import SearchBar from './SearchBar';
 
 export default function Header({ setLocation, timezone, showSidebar, setShowSidebar }) {
-    const [currentTime, setCurrentTime] = useState(DateClass.currentTime({ timezone }));
+  const [currentTime, setCurrentTime] = useState(DateClass.currentTime({ timezone }));
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            const newTime = DateClass.currentTime({ timezone });
-            setCurrentTime(newTime);
-        }, 1000);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newTime = DateClass.currentTime({ timezone });
+      setCurrentTime(newTime);
+    }, 1000);
 
-        return () => clearInterval(intervalId);
-    }, [timezone]);
+    return () => clearInterval(intervalId);
+  }, [timezone]);
 
-    const handleShowSidebar = (e) => {
-        console.log('click open');
-        e.classList.toggle('change');
-        setShowSidebar(true);
-    };
+  const handleToggleSidebar = (e) => {
+    setShowSidebar(!showSidebar);
+  };
 
-    const handleHideSidebar = (e) => {
-        console.log('click close');
-        e.classList.toggle('change');
-        setShowSidebar(false);
-    };
-
-    return (
-        <div className='header-container'>
-            <header>
-                <div
-                    className='hamburger-bars'
-                    onClick={(e) => {
-                        showSidebar ? handleHideSidebar(e) : handleShowSidebar(e);
-                    }}>
-                    <div className='bar1'></div>
-                    <div className='bar2'></div>
-                    <div className='bar3'></div>
-                </div>
-                <h1 className='app-title'>Weather App</h1>
-                <h1 className='current-time'>{currentTime}</h1>
-                <SearchBar setLocation={setLocation} />
-            </header>
-            <hr />
+  return (
+    <div className='header-container'>
+      <header>
+        <div
+          className='hamburger-bars'
+          onClick={handleToggleSidebar}
+        >
+          <div className={`bar1 ${showSidebar ? 'bar1-change' : ''}`}></div>
+          <div className={`bar2 ${showSidebar ? 'bar2-change' : ''}`}></div>
+          <div className={`bar3 ${showSidebar ? 'bar3-change' : ''}`}></div>
         </div>
-    );
+        <h1 className='app-title'>Weather App</h1>
+        <h1 className='current-time'>{currentTime}</h1>
+        <SearchBar setLocation={setLocation} />
+      </header>
+      <hr />
+    </div>
+  );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
 export default function Sidebar({ favoriteCities }) {
-    return (
-        <div className='sidebar-container'>
-            <div className='favorite-cities'>
-                <li>
-                    <ul>Washington</ul>
-                    <ul>Chicago</ul>
-                    <ul>Seahaven</ul>
-                </li>
-            </div>
-            <div className='other-pages'>
-                <p>Settings</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className='sidebar-container'>
+      <div className='favorite-cities'>
+        <ul>
+          <li>Washington</li>
+          <li>Chicago</li>
+          <li>Seahaven</li>
+        </ul>
+      </div>
+      <div className='other-pages'>
+        <p>Settings</p>
+      </div>
+    </div>
+  );
 }
