00f5a701a5ab0b74ca81d24c696885a39b52ca4f
lunargravity <anuuhai2000@gmail.com>
2024-08-20 14:43:37 -0400

    Validation for user location query

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function SearchBar({ setLocation }) {
   const [query, setQuery] = useState('');
   const key = '3287679688d14c97ad8392d7fa3e2af3';
-  const url = `https://api.opencagedata.com/geocode/v1/json?q=${query}&key=${key}`;
 
   const onSearch = async (e) => {
     e.preventDefault();
-    if (query) {
+    const trimmedQuery = query.trim();
+
+    if (trimmedQuery) {
+      const url = `https://api.opencagedata.com/geocode/v1/json?q=${trimmedQuery}&key=${key}`;
       try {
         const response = await fetch(url);
         const data = await response.json();
-        if (data.results) {
+        if (data.results && data.results.length > 0) {
           setLocation({
             address: data.results[0].formatted,
             latitude: data.results[0].geometry.lat,
@@ -19,13 +21,22 @@ export default function SearchBar({ setLocation }) {
             timezone: data.results[0].annotations.timezone.name,
           });
         } else {
-          setLocation('Invalid location');
+          setLocation({
+            address:
+              'Needmore, San Augustine County, Texas, United States of America',
+            latitude: 31.1324149,
+            longitude: -94.0535239,
+            timezone: 'America/Chicago',
+          });
         }
       } catch (error) {
-        setLocation('Fetching error');
+        setLocation({
+          address: 'Nowhere, Caddo County, Oklahoma, United States of America',
+          latitude: 35.1592,
+          longitude: 98.4423,
+          timezone: 'America/Chicago',
+        });
       }
-    } else {
-      console.log('there was an error with your search');
     }
   };
 
